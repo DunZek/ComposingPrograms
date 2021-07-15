504b89543052a174eca10cfd0607ca9007ef5109
DunZek <60448983+DunZek@users.noreply.github.com>
2021-07-15 09:53:23 -0600

    first

--- 1_buildingAbstractionsWithFunctions/1_gettingStarted.py
+++ 1_buildingAbstractionsWithFunctions/1_gettingStarted.py
@@ -1,1 +1,13 @@
-#
+""" 1.1 Getting Started
+    - All computing begins with:
+        1. Representing information
+        2. Specifying logic to process it
+        3. Desigining abstractions that manage the complexity of that logic
+"""
+# You must learn how computers interpret programs and carry out the computational process
+
+""" 1.1.1 Programming in Python
+    - Python will be used to define computational processes because both man and machine can understand it
+    - The language is a large open-source project
+        - It was started by Guido van Rossum in the late 80s
+"""
--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 My personal learning
 
 Link:
-- <
+- <http://www.composingprograms.com/>
